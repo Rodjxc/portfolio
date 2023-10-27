8f229b7e6857fbbee5a0ba77380fa34cfd7ee9b2
Rod <rodrysxc@gmail.com>
2023-10-27 12:37:26 +0200

    Finished with the display, css and routing of portfolio section

--- src/Data/Works.tsx
+++ src/Data/Works.tsx
@@ -6,17 +6,17 @@ export const Works = [
     tecnologies: "HTML, React, Typescript & Tailwind CSS",
     categories: "Web Design",
     description:
-      "A website for a fake music festival, built using React and Tailwind CSS.",
+      "A website for a fake music festival, built using React and Tailwind CSS and implementing React libraries .",
     github: "https://github.com/Rodjxc/Music-Festival",
   },
   {
     id: "movietrack",
     name: "MovieTrack",
-    url: "movietrack.com",
-    tecnologies: "HTML, CSS, React & Typescript",
+    url: "https://movies-site-h4l5b12fe-rodjxc.vercel.app",
+    tecnologies: "HTML, CSS, React & JS",
     categories: "Web Design",
     description:
-      "A website to write down, rate, and track movies you've watched or want to watch and add a description about them. Built using React.",
+      "A website to write down, rate, and track movies you've watched or want to watch and add a description about them. Built using React and saving the items in the Local Storage",
     github: "https://github.com/Rodjxc/movies-site",
   },
   {
