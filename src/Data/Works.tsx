1ba03be3acb99ec7c03cf2de857625a67276bc23
Rod <rodrysxc@gmail.com>
2023-11-09 11:59:25 +0100

    feat: added the portfolio to the projects section

--- src/Data/Works.tsx
+++ src/Data/Works.tsx
@@ -4,7 +4,7 @@ export const Works = [
     name: "Music Festival",
     url: "https://music-festival-ten.vercel.app/",
     tecnologies: "HTML, React, Typescript & Tailwind CSS",
-    categories: "Web Design",
+    categories: "Website",
     description:
       "A website for a fake music festival, built using React and Tailwind CSS and implementing React libraries .",
     github: "https://github.com/Rodjxc/Music-Festival",
@@ -14,10 +14,19 @@ export const Works = [
     name: "MovieTrack",
     url: "https://movies-site-h4l5b12fe-rodjxc.vercel.app",
     tecnologies: "HTML, CSS, React & JS",
-    categories: "Web Design",
+    categories: "Website functionality",
     description:
       "A website to write down, rate, and track movies you've watched or want to watch and add a description about them. Built using React and saving the items in the Local Storage",
     github: "https://github.com/Rodjxc/movies-site",
+  },   {
+    id: "portoflio",
+    name: "Portfolio",
+    url: "https://rodj.dev",
+    tecnologies: "HTML, CSS, React & TS",
+    categories: "Website",
+    description:
+      "This same website, where I got to play a bit more with React and other functionalities and, at the same time, building my own portfolio. Built with the React Routing library.",
+    github: "https://github.com/Rodjxc/portfolio",
   },
   {
     id: "lafogataderaid",
@@ -26,6 +35,6 @@ export const Works = [
     tecnologies: "Wordpress, CSS",
     categories: "Web Design",
     description:
-      "My personal project, the first functional website I created for the videogame Raid Shadow Legends. It's the reference in Spanish of game guides of the videogame.",
+      "My personal project, the first functional website I created for the videogame Raid Shadow Legends. It's the reference in Spanish language for the game Raid Shadow Legends videogame.",
   },
 ];
