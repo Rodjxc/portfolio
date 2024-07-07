27fd0a5a33aeeea7170db6c369f813debde17a8b
Rod <rodrysxc@gmail.com>
2024-07-07 18:49:39 +0200

    fix: fix issue that preventing from compiling after changing tecnologies for technologies

--- src/Components/Project/Project.tsx
+++ src/Components/Project/Project.tsx
@@ -2,25 +2,10 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { Works } from "../../Data/Works";
 import { Box, Heading, Image, Text, Link, VStack } from "@chakra-ui/react";
-
-// Here we create an interface to let the useState know that the params we'll be receiving, will be of those types
-interface ProjectType {
-  id: string;
-  name: string;
-  tecnologies: string;
-  description: string;
-  url: string;
-}
+import { ProjectType } from "./projectTypes";
 
 export const Project = () => {
-  // Here we use the useParams to get the parameters like id, descript, etc from the element we're reading (in this case, from
-  // the items of the portfolio)
-
   const params = useParams();
-
-  // And here, linked to the interface, we define the initial state of the useState depending on the parameter we receive. It'll be
-  // of one of those types, or it'll be null
-
   const [proyecto, setProyecto] = useState<ProjectType | null>(null);
 
   useEffect(() => {
@@ -51,7 +36,7 @@ export const Project = () => {
           <Heading as="h1" size="xl">
             {proyecto.name}
           </Heading>
-          <Text fontSize="lg">{proyecto.tecnologies}</Text>
+          <Text fontSize="lg">{proyecto.technologies}</Text>
           <Text>{proyecto.description}</Text>
           <Link href={proyecto.url} isExternal color="teal.500">
             Go to the Project
---
+++ src/Components/Project/projectTypes.ts
@@ -0,0 +1,7 @@
+export type ProjectType = {
+  id: string;
+  name: string;
+  technologies: string;
+  description: string;
+  url: string;
+};
