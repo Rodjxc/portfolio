eef1c2c9e1a90bd6e56a5b2d9563858b4cc2e6cb
Rod <rodrysxc@gmail.com>
2024-08-04 23:26:03 +0200

    refactor: separate components from pages

--- src/Router/MyRoutes.tsx
+++ src/Router/MyRoutes.tsx
@@ -1,43 +1,43 @@
 import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
-import { Home } from "../Components/Home/Home";
-import { Portfolio } from "../Components/Portfolio/Portfolio";
-import { CV } from "../Components/CV/CV";
-import { HeaderNav } from "../Components/layout/HeaderNav/HeaderNav";
-import { Footer } from "../Components/layout/Footer/Footer";
-import { Project } from "../Components/Project/Project";
+import { Home } from "../Pages/Home/Home";
+import { Portfolio } from "../Pages/Portfolio/Portfolio";
+import { CV } from "../Pages/CV/CV";
+import { HeaderNav } from "../Pages/layout/HeaderNav/HeaderNav";
+import { Footer } from "../Pages/layout/Footer/Footer";
+import { Project } from "../Pages/Project/Project";
 
 export const MyRoutes = () => {
-  return (
-    <div>
-      <BrowserRouter>
-        {/*   HEADER AND NAVIGATION */}
-        <HeaderNav />
-        {/*   MAIN CONTENT */}
-        <section className="content">
-          <Routes>
-            <Route path="/" element={<Navigate to="/home" />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/CV" element={<CV />} />
-            <Route path="/project/:id" element={<Project />} />
+	return (
+		<div>
+			<BrowserRouter>
+				{/*   HEADER AND NAVIGATION */}
+				<HeaderNav />
+				{/*   MAIN CONTENT */}
+				<section className="content">
+					<Routes>
+						<Route path="/" element={<Navigate to="/home" />} />
+						<Route path="/home" element={<Home />} />
+						<Route path="/portfolio" element={<Portfolio />} />
+						<Route path="/CV" element={<CV />} />
+						<Route path="/project/:id" element={<Project />} />
 
-            <Route
-              path="*"
-              element={
-                <div className="page">
-                  <h2 className="heading">
-                    Error 404. The content doesn't exist or couldn't be
-                    displayed
-                  </h2>
-                </div>
-              }
-            ></Route>
-          </Routes>
-        </section>
+						<Route
+							path="*"
+							element={
+								<div className="page">
+									<h2 className="heading">
+										Error 404. The content doesn't exist or couldn't be
+										displayed
+									</h2>
+								</div>
+							}
+						/>
+					</Routes>
+				</section>
 
-        {/*   FOOTER  */}
-        <Footer />
-      </BrowserRouter>
-    </div>
-  );
+				{/*   FOOTER  */}
+				<Footer />
+			</BrowserRouter>
+		</div>
+	);
 };
