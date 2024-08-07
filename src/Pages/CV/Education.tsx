04639e1168149665a31ad5868eaa0a9857bd54d3
Rod <rodrysxc@gmail.com>
2024-08-07 16:03:37 +0200

    refactor: update Education component to match styles

--- src/Pages/CV/Education.tsx
+++ src/Pages/CV/Education.tsx
@@ -1,20 +1,43 @@
-import { Heading, Link, List, ListItem } from "@chakra-ui/react";
+import { Box, Link, List, ListItem } from "@chakra-ui/react";
 import { COLORS } from "../../common/colors";
 import { educationItems } from "./educationData";
+import { motion } from "framer-motion";
+
+const MotionBox = motion(Box);
 
 export const Education = () => {
 	return (
-		<div className="flex flex-col items-center py-6">
-			<Heading
-				className="text-pink-custom"
-				fontWeight="400"
-				fontSize="40px"
-				textTransform="uppercase"
-				mb="20"
+		<Box
+			className="flex flex-col items-center py-6"
+			px={{ base: "4", md: "20" }}
+		>
+			<MotionBox
+				fontWeight="bold"
+				fontSize={{ base: "40px", md: "90px" }}
 				textAlign="center"
+				color={COLORS.PINK}
+				my={{ base: 10, md: 20 }}
+				fontFamily="'Montserrat', sans-serif"
 			>
 				Education
-			</Heading>
+			</MotionBox>
+
+			<MotionBox
+				fontSize={{ base: "80px", md: "200px" }}
+				fontWeight="bold"
+				color="rgba(255, 255, 255, 0.1)"
+				position="absolute"
+				top="50%"
+				left="50%"
+				transform="translate(-50%, -50%)"
+				whiteSpace="nowrap"
+				pointerEvents="none"
+				zIndex={-1}
+				fontFamily="'Montserrat', sans-serif"
+			>
+				Education
+			</MotionBox>
+
 			<List>
 				{educationItems.map((item, index) => (
 					<ListItem key={index} mb="15px">
@@ -29,6 +52,6 @@ export const Education = () => {
 					</ListItem>
 				))}
 			</List>
-		</div>
+		</Box>
 	);
 };
