096df69bb5e9e73eb2d98560eb8dde41914d8935
Rod <rodrysxc@gmail.com>
2024-08-08 10:37:12 +0200

    refactor: update also About Me section and fix merge

--- src/Pages/AboutMe/AboutMe.tsx
+++ src/Pages/AboutMe/AboutMe.tsx
@@ -1,4 +1,13 @@
-import { Heading, Link, Text } from "@chakra-ui/react";
+import {
+	Box,
+	Flex,
+	Heading,
+	Text,
+	Link,
+	useMediaQuery,
+} from "@chakra-ui/react";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { COLORS } from "../../common/colors";
 import {
 	faGithub,
 	faInstagram,
@@ -7,76 +16,141 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+const MotionBox = motion(Box);
+
 export const AboutMe = () => {
+	const { scrollY } = useScroll();
+
+	// Define media queries
+	const [isLargerThanMd] = useMediaQuery("(min-width: 48em)");
+
+	// Side to side scroll effect for the background text
+	const backgroundTextX = useTransform(scrollY, [0, 500], [-300, 800]);
+
+	// Translate left effect for the "About Me" title (only on larger screens)
+	const titleTextX = useTransform(
+		scrollY,
+		[0, 500],
+		isLargerThanMd ? [0, -150] : [0, 0],
+	);
+
 	return (
-		<div className="bg-lightBlack-custom p-28 my-4">
-			<Heading
-				className="text-pink-custom"
-				fontWeight="400"
-				fontSize="60px"
-				textTransform="uppercase"
-				mb="20"
-				textAlign="center"
+		<Box
+			position="relative"
+			overflow="hidden"
+			className="bg-lightBlack-custom px-8 mb-2"
+			pt={{ base: "20%", md: "10%" }} // Increased top padding for mobile
+			mb={{ base: "10%", md: "5%" }} // Increased bottom margin
+		>
+			<MotionBox
+				style={{ x: backgroundTextX }}
+				fontSize={{ base: "50px", md: "180px" }}
+				fontWeight="bold"
+				color="rgba(255, 255, 255, 0.1)"
+				position="absolute"
+				top={{ base: "50%", md: "70%" }}
+				left={{ base: "50%", md: "20%" }}
+				transform="translate(-50%, -50%)"
+				whiteSpace="nowrap"
+				zIndex={0}
+				pointerEvents="none"
 			>
 				About Me
-			</Heading>
-			<Text className="text-white">
-				Since I was 5 I've been surrounded by computers and videogames, which
-				probably laid the foundations of an interest for technology. This
-				interest took me to a pivotal decision shifting from a 10+ years working
-				in the craft beer industry and being a beer sommelier, to changing my
-				career to become a developer.
-			</Text>
-			<br />
-			<Text className="text-white">
-				I believe that my brain naturally works exactly the way a developer
-				thinks, with the logic side of programming and believing that there's
-				always more than one solution to a problem. Before I used my creativity
-				to make original and tasty beers and now I use it to create web
-				applications. I really enjoy minimalist designs inspired by the likes of
-				Next JS and Vercel. When I see a project built with frameworks I'm not
-				familiar with, I instantly get an “I want to learn this” feeling
-			</Text>
-			<br />
-			<Text className="text-white">
-				When I'm not at the computer I'm usually in the gym practicing Brazilian
-				Jiu Jitsu, travelling or spending time with my wife and dog
-			</Text>{" "}
-			<br />
-			<Text className="text-white">
-				Oh, and I also have a YouTube channel where I make guides for
-				videogames!
-			</Text>
-			<div className="mt-20">
-				<Link
-					href="https://github.com/Rodjxc"
-					target="_blank"
-					className="text-lg mx-3 pb-2 text-white  hover:text-pink-custom hover:border-pink-custom"
+			</MotionBox>
+			<Flex
+				direction="column"
+				alignItems={{ base: "center", md: "flex-start" }}
+				justifyContent="left"
+				paddingX={{ base: "2%", md: "5%" }}
+				paddingY={{ base: "4%", md: "2%" }}
+				position="relative"
+				zIndex={1}
+			>
+				<MotionBox
+					style={{ x: titleTextX }}
+					fontSize={{ base: "16px", md: "20px" }}
+					lineHeight="30px"
+					paddingX={{ base: "0", md: "20%" }}
+					mb="20"
+					textAlign="left"
 				>
-					<FontAwesomeIcon icon={faGithub} className="size-6 text-white" />
-				</Link>
-				<Link
-					href="https://www.linkedin.com/in/rod-jimeno/"
-					target="_blank"
-					className="text-lg mx-3 pb-2 text-white  hover:text-pink-custom hover:border-pink-custom"
-				>
-					<FontAwesomeIcon icon={faLinkedinIn} className="size-6 text-white" />
-				</Link>
-				<Link
-					href="https://www.instagram.com/sendaplays/"
-					target="_blank"
-					className="text-lg mx-3 pb-2 text-white  hover:text-pink-custom hover:border-pink-custom"
-				>
-					<FontAwesomeIcon icon={faInstagram} className="size-6 text-white" />
-				</Link>
-				<Link
-					href="https://www.youtube.com/@SendaPlays"
-					target="_blank"
-					className="text-lg mx-3 pb-2 text-white  hover:text-pink-custom hover:border-pink-custom"
-				>
-					<FontAwesomeIcon icon={faYoutube} className="size-6 text-white" />
-				</Link>
-			</div>
-		</div>
+					<Heading
+						mb="12"
+						fontSize={{ base: "40px", md: "100px" }}
+						fontWeight="bold"
+						color={COLORS.PINK}
+						fontFamily="'Montserrat', sans-serif"
+						letterSpacing="normal"
+					>
+						About Me
+					</Heading>
+					<Text className="text-white">
+						Since I was 5 I've been surrounded by computers and videogames,
+						which probably laid the foundations of an interest in technology.
+						This interest took me to a pivotal decision, shifting from a 10+
+						years working in the craft beer industry and being a beer sommelier,
+						to changing my career to become a developer.
+					</Text>
+					<br />
+					<Text className="text-white">
+						I believe that my brain naturally works exactly the way a developer
+						thinks, with the logic side of programming and believing that
+						there's always more than one solution to a problem. Before I used my
+						creativity to make original and tasty beers and now I use it to
+						create web applications. I really enjoy minimalist designs inspired
+						by the likes of Next JS and Vercel. When I see a project built with
+						frameworks I'm not familiar with, I instantly get an “I want to
+						learn this” feeling.
+					</Text>
+					<br />
+					<Text className="text-white">
+						When I'm not at the computer I'm usually in the gym practicing
+						Brazilian Jiu Jitsu, travelling, or spending time with my wife and
+						dog.
+					</Text>
+					<br />
+					<Text className="text-white">
+						Oh, and I also have a YouTube channel where I make guides for
+						videogames!
+					</Text>
+					<div className="mt-20">
+						<Link
+							href="https://github.com/Rodjxc"
+							target="_blank"
+							className="text-lg mx-3 pb-2 text-white  hover:text-pink-custom hover:border-pink-custom"
+						>
+							<FontAwesomeIcon icon={faGithub} className="size-6 text-white" />
+						</Link>
+						<Link
+							href="https://www.linkedin.com/in/rod-jimeno/"
+							target="_blank"
+							className="text-lg mx-3 pb-2 text-white  hover:text-pink-custom hover:border-pink-custom"
+						>
+							<FontAwesomeIcon
+								icon={faLinkedinIn}
+								className="size-6 text-white"
+							/>
+						</Link>
+						<Link
+							href="https://www.instagram.com/sendaplays/"
+							target="_blank"
+							className="text-lg mx-3 pb-2 text-white  hover:text-pink-custom hover:border-pink-custom"
+						>
+							<FontAwesomeIcon
+								icon={faInstagram}
+								className="size-6 text-white"
+							/>
+						</Link>
+						<Link
+							href="https://www.youtube.com/@SendaPlays"
+							target="_blank"
+							className="text-lg mx-3 pb-2 text-white  hover:text-pink-custom hover:border-pink-custom"
+						>
+							<FontAwesomeIcon icon={faYoutube} className="size-6 text-white" />
+						</Link>
+					</div>
+				</MotionBox>
+			</Flex>
+		</Box>
 	);
 };
--- src/Pages/TechStack/WorkingTools.tsx
+++ src/Pages/TechStack/WorkingTools.tsx
@@ -1,9 +1,6 @@
 import { Box, Heading, List, ListItem, Image } from "@chakra-ui/react";
 import { motion, useInView } from "framer-motion";
 import { useRef } from "react";
-import { Box, Heading, List, ListItem, Image } from "@chakra-ui/react";
-import { motion, useInView } from "framer-motion";
-import { useRef } from "react";
 
 const workingTools = [
 	"Github",
@@ -15,83 +12,17 @@ const workingTools = [
 	"Spotify",
 	"Adobe Illustrator",
 	"and Google!",
-	"Adobe Illustrator",
-	"and Google!",
 ];
 
 const MotionListItem = motion(ListItem);
-const MotionBox = motion(Box);
 const MotionBox = motion(Box);
 
 export const WorkingTools = () => {
 	const ref = useRef(null);
 	const isInView = useInView(ref, { once: true });
 
-	const ref = useRef(null);
-	const isInView = useInView(ref, { once: true });
-
 	return (
 		<Box
-			position="relative"
-			overflow="hidden"
-			maxW={{ base: "90%", md: "70%" }}
-			mx="auto"
-			my="8"
-			height="300px"
-		>
-			<Image
-				src="/img/tools.jpg"
-				alt="Working Tools"
-				width="100%"
-				objectFit="cover"
-				position="absolute"
-				top="0"
-				left="0"
-				zIndex="1"
-				height="100%"
-				opacity="0.5"
-			/>
-			<MotionBox
-				ref={ref}
-				bg="rgba(255, 255, 255, 0.9)"
-				borderRadius="md"
-				p="6"
-				position="relative"
-				zIndex="2"
-				mx="auto"
-				my="5%"
-				boxShadow="md"
-				textAlign="center"
-				maxWidth={{ base: "90%", md: "60%" }}
-				initial={{ opacity: 0, x: -50 }}
-				animate={isInView ? { opacity: 1, x: 0 } : {}}
-				transition={{ duration: 0.6 }}
-			>
-				<Heading
-					fontWeight="700"
-					fontSize={{ base: "18px", md: "24px" }}
-					mb="4"
-					color="black"
-					fontFamily="'Montserrat', sans-serif"
-				>
-					Working Tools
-				</Heading>
-				<List color="black" fontSize="sm">
-					<Box justifyItems="center">
-						{workingTools.map((tool, index) => (
-							<MotionListItem
-								key={index}
-								initial={{ opacity: 0, y: 20 }}
-								animate={isInView ? { opacity: 1, y: 0 } : {}}
-								transition={{ delay: index * 0.1 }}
-								textAlign="center"
-							>
-								{tool}
-							</MotionListItem>
-						))}
-					</Box>
-				</List>
-			</MotionBox>
 			position="relative"
 			overflow="hidden"
 			maxW={{ base: "90%", md: "70%" }}
