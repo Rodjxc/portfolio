99e195e9c0445139119a479ef154f76738deae50
Rod <rodrysxc@gmail.com>
2024-08-26 16:51:39 +0200

    refactor: fix error with class on div

--- src/Pages/layout/Home/Home.tsx
+++ src/Pages/layout/Home/Home.tsx
@@ -1,14 +1,15 @@
 import { Staff } from "../../Staff/Staff";
 import { TechStack } from "../../TechStack/TechStack";
 import { Hero } from "../../Hero/Hero";
+import { Box } from "@chakra-ui/react";
 
 export const Home = () => {
 	return (
 		<div className="bg-lightBlack-custom text-white">
 			<Hero />
-			<div mt={{ base: "10%", md: "5%" }}>
+			<Box mt={{ base: "10%", md: "5%" }}>
 				<TechStack />
-			</div>
+			</Box>
 			<Staff />
 		</div>
 	);
